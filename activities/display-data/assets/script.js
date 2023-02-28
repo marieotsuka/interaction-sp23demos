fba503904e306253fa737a30523251c48d4c5ee1
Marie Otsuka <motsuka.gd@gmail.com>
2023-02-28 18:04:45 -0500

    Update script.js

--- activities/display-data/assets/script.js
+++ activities/display-data/assets/script.js
@@ -1,7 +1,9 @@
-
-// First show all the flowers and create divs for each of them
+//setup container element
 let container = document.getElementById("container");
-// create a counter variable that will give us each flowers number in the array
+
+// must setup a local server to use fetch
+// see Python instructions here:
+// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server#using_python
 
 fetch('./assets/emojis.json')
   .then(response => response.json())
