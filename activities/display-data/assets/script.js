dc66953bbfaa6ce15983d711dbc237076b11cd6e
Marie Otsuka <motsuka.gd@gmail.com>
2023-02-28 20:27:51 -0500

    upload in-class demo code

--- activities/display-data/assets/script.js
+++ activities/display-data/assets/script.js
@@ -1,26 +1,55 @@
+// console.log('hi!');
+
+let letters = ['a', 'b', 'c']; //an array;
+
+let emojiObject = {
+    "by": "Emily",
+    "id": 13,
+    "emoji": "👍",
+    "img": "",
+    "description": "Thumb's up",
+    "alt-meaning": 'indicates "" approval',
+    "category": "hand symbols",
+    "year": 2010,
+    "update": "unicode 6.0",
+    "general-usage": 5,
+    "personal-usage": 3,
+    "intensity": 4,
+    "sample": "sounds good",
+    "unicode": "U+1F44D"
+  };
 //setup container element
 let container = document.getElementById("container");
 
 // must setup a local server to use fetch
 // see Python instructions here:
 // https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server#using_python
-
 fetch('./assets/emojis.json')
   .then(response => response.json())
   .then(data => {
-    displayData(data);
+    console.log(data);
+    processEmojis(data);
   })
   .catch(error => console.log(error));
 
-function displayData( data ){
+
+function sayHello(){
+  console.log('Hello');
+}
+
+function processEmojis( data ){
   data.forEach( function(item, index){
-    // console.log(item, index);
+    console.log(item, index);
     let newItem = document.createElement("div");
     newItem.classList.add("icon");
     newItem.innerHTML = `
+      <!--commenting out <div class="image"><img src="assets/images/${item.img}.jpg"></div>-->
+      <div class="usage">${item['general-usage']}</div>
       <div class="phrase">${item.sample}</div>
       <div class="category">${item.category}</div>
       <div class="emoji">${item.emoji}</div>`;
     container.appendChild(newItem);    
   });
 }
+
+sayHello();
