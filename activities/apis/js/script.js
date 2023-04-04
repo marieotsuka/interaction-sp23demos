aac46e6f5d5d48dd44b45cd481630771546abb3e
Marie Otsuka <motsuka.gd@gmail.com>
2023-04-04 16:48:14 -0400

    APIs demo

---
+++ activities/apis/js/script.js
@@ -0,0 +1,40 @@
+
+let factList;
+
+function getFact(){
+  fetch('https://cat-fact.herokuapp.com/facts/')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      factList = data; // store data
+      displayFact(0);
+    })
+    .catch(error => console.log(error));
+}
+
+function displayFact(factIndex){
+  let fact = factList[factIndex];
+  let text = fact.text;
+  document.getElementById('fact').innerHTML = text;
+}
+
+
+window.onload = function(e){ 
+
+    let factCount = 0;
+
+    getFact();
+
+    let refreshButton = document.getElementById('refresh');
+    refreshButton.addEventListener('click', function(){
+
+     if(factCount < 4){        
+        factCount ++; // increment fact index until 5
+        displayFact(factCount);
+      }else{
+        factCount = 0; // reset fact index
+      }  
+       
+    });
+
+};
